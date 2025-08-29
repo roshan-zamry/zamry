563136d40a2247d39cc7e55cade19b9dfab7c51f
Roshan Zamry <roshan@cxunicorn.com>
2025-08-29 16:10:47 +0400

    pt-20

--- src/app/myproject/page.tsx
+++ src/app/myproject/page.tsx
@@ -207,7 +207,7 @@ export default function ProjectsShowcase() {
     <div id="projects" className="bg-black py-16 sm:py-20 px-4">
       <Navbar />
 
-      <div className="max-w-7xl mx-auto">
+      <div className="max-w-7xl mx-auto pt-20">
         <motion.div
           initial={{ opacity: 0, y: -20 }}
           animate={{ opacity: 1, y: 0 }}
