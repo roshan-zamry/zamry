db000c636e9c8a803b5467d8fc5d8f4c0986e36c
roshan-zamry <60825909+roshan-zamry@users.noreply.github.com>
2026-03-10 12:50:01 +0000

    change

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -263,7 +263,7 @@ const Hero = () => {
                 />
               </svg>
               <a
-                href="/cv/public/cv/ROSHAN ZAMRY.pdf"
+                href="/cv/public/cv/roshanzamry.pdf"
                 target="_blank"
                 rel="noopener noreferrer"
               >
